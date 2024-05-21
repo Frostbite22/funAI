21d19d3eb13da6d11e0047e93995afb1c5705c70
Frostbite22 <landoulsiferes@outlook.com>
2024-05-21 10:52:46 +0200

    dataloader and split data

--- scone_demo/dataloader.py
+++ scone_demo/dataloader.py
@@ -10,3 +10,26 @@ def load_scone(dirname):
         df['category'] = os.path.basename(filename).replace('.csv','')
         dfs.append(df)
     data_df = pd.concat(dfs)
+
+    def as_example(row):
+        # the "one_scoped" file is from an earlier dataset, MoNLI
+        # so is formatted a bit differently
+        suffix = '' if row['category'] == 'one_scoped' else '_edited'
+        # Reformat the Hypothesis to be an embedded clause in a question
+        hkey = 'sentence2' + suffix
+        question = row[hkey][0].lower() + row[hkey][1:].strip(".") 
+        question = f"Can we logically conclude for sure that {question}?"
+        # binary task reformulation:
+        label = "Yes" if row['gold_label' + suffix] == 'entailment' else "No"
+        return dspy.Example({
+            "context" : row['sentence1' + suffix],
+            "question" : question,
+            "answer" : label,
+            "category" : row['category']
+        }
+        ).with_inputs('context','question')
+    return list(data_df.apply(as_example, axis=1).values)
+
+
+
+
--- scone_demo/llm_config.py
+++ scone_demo/llm_config.py
@@ -7,7 +7,20 @@ import dspy
 from dspy.evaluate import Evaluate
 from dspy.teleprompt import BootstrapFewShotWithRandomSearch
 
+from dataloader import load_scone
+
 llama3 = dspy.GROQ(model='llama3-8b-8192', api_key=os.environ.get("GROQ_API_KEY"))
 dspy.settings.configure(lm=llama3)
+
+all_train = load_scone("ScoNe/scone_nli/train")
+
+random.seed(1)
+random.shuffle(all_train)
+
+# 200 random train, 50 random dev
+train, dev = all_train[:200], all_train[200:250] 
+
+print(len(train), len(dev))
+
 
 
